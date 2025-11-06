b42d1d5f266e0156e22393a3fed343c320b003bb
Kaushal <077bct020.kaushal@sagarmatha.edu.np>
2025-11-06 09:42:02 +0545

    feat: integrate Arcjet middleware for enhanced security and rate limiting

--- config/env.js
+++ config/env.js
@@ -2,4 +2,4 @@ import { config } from "dotenv";
 
 config({path: `.env.${process.env.NODE_ENV || "development"}.local`});
 
-export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+export const { PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_KEY, ARCJET_ENV } = process.env;
---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,18 @@
+import aj from '../config/arcjet.js';
+
+export const arcjetMiddleware = async (req, res, next) => {
+    try { 
+        const decision = await aj.protect(req, {requested:1});
+
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) return res.status(429).json({ error: 'Too Many Requests - Rate limit exceeded'});
+            if (decision.reason.isBot()) return res.status(403).json({error: 'Access Denied - Bot detected'}); 
+            
+            return res.status(403).json({error:'Access Denied'});
+        }
+        next();
+    } catch (error) {
+        console.log(`Arcjet Middleware Error: ${error}`);
+        next(error);
+    }
+}
